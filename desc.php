5a021633f2cb1b12e693e911b74ff06e7e4ad147
Clement-Loth <74249177+Clement-Loth@users.noreply.github.com>
2022-02-23 18:06:44 +0100

    desc

---
+++ desc.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+<head>
+<?php
+
+// $paint = mysql_query("SELECT * FROM paint WHERE ")
+
+?>
+
+
+<body>
+	<ul class="list-group"><h1> <?php echo("Peinture Noir Mat")?></h1>
+	
+		<li class ="list-group-item list-group-item-secondary"><img src="https://media.peinturevoiture.fr/7043-large_default/peinture-urethane-mate.jpg" class="rounded"></li>
+		<li class ="list-group-item list-group-item-secondary">
+			<h1 class="display-6">Description :</h1>
+			<?php echo("Ceci est une description précise ")?>
+	
+		</li>
+		<li class ="list-group-item list-group-item-secondary">
+			<h1 class="display-7"> PRIX : 25€</h1>
+		</li>
+	
+	
+	</ul>
+	
+
+
+
+
+</body>
+</head>
+</html>

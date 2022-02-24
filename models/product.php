ba6cdabb21b5ef9283242749abbe9c7d7df3f539
Negikami <45533155+Negikami@users.noreply.github.com>
2022-02-24 19:13:09 +0100

    ajout shade_id

--- models/product.php
+++ models/product.php
@@ -9,6 +9,7 @@ class product{
 
     public function __construct(int $id, int $shade_id, string $name, float $price, ?string $description, ?int $quantity){
         $this->id = $id;
+		$this->$shade_id = $shade_id;
         $this->name = $name;
         $this->price = $price;
         $this->description = $description ?? "";

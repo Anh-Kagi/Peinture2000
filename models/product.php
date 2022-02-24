9bd0d0d028523d9993ee002624f71dedd0c7d937
Negikami <45533155+Negikami@users.noreply.github.com>
2022-02-24 19:44:54 +0100

    Update product.php

--- models/product.php
+++ models/product.php
@@ -1,15 +1,15 @@
 <?php
 class Product{
-    public  $id;
-    public  $shade_id;
-    public  $name;
-    public  $price;
-    public  $description;
-    public  $quantity;
+    public int $id;
+    public int $shade_id;
+    public string $name;
+    public float $price;
+    public string $description;
+    public int $quantity;
 
     public function __construct(int $id, int $shade_id, string $name, float $price, ?string $description, ?int $quantity){
         $this->id = $id;
-		$this->shade_id = $shade_id;
+        $this->shade_id = $shade_id;
         $this->name = $name;
         $this->price = $price;
         $this->description = $description ?? "";

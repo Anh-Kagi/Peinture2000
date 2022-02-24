0073391d2ac526aad704c711b67423c2e6df73f9
Negikami <45533155+Negikami@users.noreply.github.com>
2022-02-24 20:37:38 +0100

    Add files via upload

--- pageAjout.php
+++ pageAjout.php
@@ -3,9 +3,31 @@ require_once  "services/productService.php";
 require_once "models/product.php";
 require_once  "services/shadeService.php";
 require_once "models/shade.php";
-if(!isset($title)){
-    $title = "Peinture2000";
+if($_SERVER['REQUEST_METHOD'] === 'POST'){
+    //update du client puis redirection vers cette page en GET
+	var_dump($_POST);
+    if(!isset ($_GET['id'])){
+        $name=$_POST['nomProduit'];
+        $prix=$_POST['prixProduit'];
+        $description=$_POST['DescriptionProduit'];
+        $q=$_POST['qte'];
+		$shade_id = $_POST['nuance'];
+        ProductService::insertProduct(new Product(0,$shade_id,$name,$prix,$description,$q));
+
+    }
+    else{
+        $currentProduit= ProductService::getProductById($_GET['id']);
+        $currentProduit->name = $_POST['nomProduit'];
+        $currentProduit->description = $_POST['DescriptionProduit'];
+        $currentProduit->quantity = $_POST['qte'];
+        $currentProduit->price = $_POST['prixProduit'];
+        $currentProduit->shade_id = $_POST['nuance'];
+        ProductService::updateProduct($currentProduit);
+    }
+	
+	header( "Location: {$_SERVER['REQUEST_URI']}", true, 303 );
 }
+
 ob_start();
 $nom = "ajouter";
 $productName="";
@@ -13,14 +35,12 @@ $productDesc="";
 $productQte="";
 $productPrix=0;
 $shades= ShadeService::getAllShades();
-
-
 if(!isset ($_GET['id'])){
 
 }
 else{
     $nom="modifier";
-    $currentProduit= ProductService::getProduitById($_GET['id']);
+    $currentProduit= ProductService::getProductById($_GET['id']);
     $productName=$currentProduit->name;
     $productDesc=$currentProduit->description;
     $productQte=$currentProduit->quantity;
@@ -31,12 +51,17 @@ else{
 ?>
 <html>
 <head>
-    <link href="bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
 <h1 class="text-center">Page d'ajout produit</h1>
 <title >Ajouter un produit </title>
-<form class="text-center border" >
+<form class="text-center border" method="post" >
+    <?php
+    if(isset($_GET['id'])){
+       echo "<input type=\"hidden\" name=\"idProduit\" id=\"idProduit\" value=\"".$_GET['id']."\">";
+
+    }
+    ?>
     <label>Nom Produit</label>
     <input type=text name="nomProduit" id="nomProduit" value="<?php echo $productName ?>" required="required"><br/>
     <label>Prix Produit</label>
@@ -55,14 +80,14 @@ else{
     <label>Quantité Produit</label>
     <input type=number name="qte" id="qte" value="<?php echo $productQte ?>" ><br/>
     <label for="colorPicker">Choisissez la couleur exacte</label>
-    <input type="color" id="colorPicker" name="nuance"><br/>
+    <input type="color" id="colorPicker" name=""><br/>
     <label for="nuance">nuance de couleur</label>
-    <select name="nuance" id="nuance">
+    <select name="nuance" id="nuance" value=<?php echo $produitNuance ?>"
         <option value="">La couleur est une nuance de </option>
         <?php
         foreach ($shades as $shade)
         {
-            echo "<option value = \" ".$shade->name." \" id=\" ".$shade->id."\"> ".$shade->name."</option>";
+            echo "<option value =  ".$shade->id." > ".$shade->name."</option>";
         }
 
         ?>
@@ -82,5 +107,3 @@ else{
 $content = ob_get_clean();
 require_once('./inc/template.php');
 ?>
-
-
